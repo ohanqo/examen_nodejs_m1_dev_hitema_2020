f9852b7ca8ade19acd5793d9d94d6faaf5dbaa7a
Antoine <gm.anqo@gmail.com>
2020-05-05 15:42:52 +0200

    feat(io): add hex to ascii converter

--- io/io.js
+++ io/io.js
@@ -1,5 +1,11 @@
-module.exports.decodeHexFileContent = (filePath) =>  {
-    return new Promise((resolve, reject) => {
-        // To be implemented!
-    });
-}
+const fs = require("fs");
+
+module.exports.decodeHexFileContent = (filePath) => {
+  return new Promise((resolve, reject) => {
+    const fileContent = fs.readFileSync(filePath).toString();
+    const decodedContent = decodeURIComponent(
+      fileContent.replace(/\s+/g, "").replace(/[0-9a-f]{2}/g, "%$&")
+    );
+    resolve(decodedContent);
+  });
+};
