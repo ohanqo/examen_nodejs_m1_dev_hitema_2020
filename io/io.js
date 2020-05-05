b48eb612704e29723e7f20a022327588fe5ed5ae
Antoine <gm.anqo@gmail.com>
2020-05-05 16:51:33 +0200

    refactor(io): use try catch

--- io/io.js
+++ io/io.js
@@ -2,11 +2,12 @@ const fs = require("fs");
 
 module.exports.decodeHexFileContent = (filePath) => {
   return new Promise((resolve, reject) => {
-    const fileContent = fs.readFileSync(filePath).toString();
-    const decodedContent = decodeURIComponent(
-      fileContent.replace(/\s+/g, "").replace(/[0-9a-f]{2}/g, "%$&")
-    );
-
-    decodedContent ? resolve(decodedContent) : reject();
+    try {
+      const fileContent = fs.readFileSync(filePath).toString();
+      const decodedContent = Buffer.from(fileContent, "hex").toString();
+      resolve(decodedContent);
+    } catch (error) {
+      reject();
+    }
   });
 };
