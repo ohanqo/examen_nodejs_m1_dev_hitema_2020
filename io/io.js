d972fef580a90340a8b3ad8cc30c6cb0fb435247
Antoine <gm.anqo@gmail.com>
2020-05-05 16:38:46 +0200

    chore(lint): resolve lint errors

--- io/io.js
+++ io/io.js
@@ -6,6 +6,7 @@ module.exports.decodeHexFileContent = (filePath) => {
     const decodedContent = decodeURIComponent(
       fileContent.replace(/\s+/g, "").replace(/[0-9a-f]{2}/g, "%$&")
     );
-    resolve(decodedContent);
+
+    decodedContent ? resolve(decodedContent) : reject();
   });
 };
