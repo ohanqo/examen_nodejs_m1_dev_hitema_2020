ab8d1f4bc8934d1b28d6f9486c78c7777dee9f90
Antoine <gm.anqo@gmail.com>
2020-05-05 14:47:36 +0200

    feat(people): update

--- api/api.js
+++ api/api.js
@@ -1,9 +1,34 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const HttpStatus = require("http-status-codes");
+const PeopleService = require("./people-service");
 
-const PeopleService = require('./people-service');
-const peopleService = new PeopleService();
 const app = express();
+const v1 = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/api/v1", v1);
+
+const peopleService = new PeopleService();
 
 // To be implemented!
+v1.put("/people/:id", (request, response) => {
+  try {
+    const id = request.params.id;
+    const people = request.body;
+
+    const potentialUpdatedPeople = peopleService.updatePeople(id, people);
+
+    if (potentialUpdatedPeople) {
+      return response.sendStatus(HttpStatus.OK);
+    } else {
+      return response.sendStatus(HttpStatus.NOT_FOUND);
+    }
+  } catch (error) {
+    response.sendStatus(HttpStatus.BAD_REQUEST);
+  }
+});
 
 module.exports = app;
--- api/people-service.js
+++ api/people-service.js
@@ -1,15 +1,24 @@
-const fs = require('fs');
+const fs = require("fs");
+const notFound = -1;
 
 module.exports = class PeopleService {
-    constructor() {
-        this.peoples = JSON.parse(fs.readFileSync(__dirname + '/people.json', 'utf8'));
-    }
+  constructor() {
+    this.peoples = JSON.parse(
+      fs.readFileSync(__dirname + "/people.json", "utf8")
+    );
+  }
 
-    updatePeople(id, people) {
-        // To be implemented!
+  updatePeople(id, people) {
+    const intId = parseInt(id, 10);
+    const peopleIndexToUpdate = this.peoples.findIndex((p) => p.id === intId);
+
+    if (peopleIndexToUpdate !== notFound) {
+      this.peoples[peopleIndexToUpdate] = people;
+      return this.peoples[peopleIndexToUpdate];
     }
-    
-    getPeople(filters) {
-        // To be implemented!
-    }
-}
+  }
+
+  getPeople(filters) {
+    // To be implemented!
+  }
+};
