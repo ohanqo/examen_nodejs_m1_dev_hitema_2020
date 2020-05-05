108bf3036259e924ae63b114f5bc998a95fb4c85
Antoine <gm.anqo@gmail.com>
2020-05-05 16:28:40 +0200

    feat(socket): add history

--- websocket/websocket-server.js
+++ websocket/websocket-server.js
@@ -1,15 +1,20 @@
-const WebSocket = require('ws')
+const WebSocket = require("ws");
 const webSocketServer = new WebSocket.Server({ port: 3003 });
-  
-webSocketServer.on('connection', webSocket => {
-    webSocket.onmessage = messageEvent => {
-        const message = messageEvent.data;
-        webSocketServer.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    };
+const messageList = [];
+
+webSocketServer.on("connection", (webSocket) => {
+  webSocket.send(JSON.stringify(messageList));
+
+  webSocket.onmessage = (messageEvent) => {
+    const message = messageEvent.data;
+    messageList.push(message);
+
+    webSocketServer.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  };
 });
 
 module.exports = webSocketServer;
